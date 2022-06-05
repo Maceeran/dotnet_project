b2e96b0ba6624bc5fda2e9a52b1ce08578a3b0ff
Maciej Truskolaski <maciej.truskolaski36@gmail.com>
2022-06-05 23:59:00 +0200

    Added Offer MVC

---
+++ WebApp/WebApp/Data/Migrations/20220605214734_CreateOffer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApp.Data.Migrations
+{
+    public partial class CreateOffer : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Offer",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    RetrievalAddress = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    VoidDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Offer", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Offer");
+        }
+    }
+}
---
+++ WebApp/WebApp/Models/Offer.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApp.Models
+{
+    public class Offer
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public string RetrievalAddress { get; set; }
+        public DateTime VoidDate { get; set; }
+    }
+}
