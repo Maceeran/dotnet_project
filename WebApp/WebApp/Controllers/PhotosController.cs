bc554912478e0be61fef51a1ac8c9a2f27605796
Maciej Truskolaski <maciej.truskolaski36@gmail.com>
2022-06-07 01:03:53 +0200

    Tweaked layout & views styling, added Photo model & controller

---
+++ WebApp/WebApp/Controllers/PhotosController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class PhotosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- WebApp/WebApp/Models/Photo.cs
+++ WebApp/WebApp/Models/Photo.cs
@@ -1,7 +1,15 @@
-﻿namespace WebApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApp.Models
 {
     public class Photo
     {
         public int Id { get; set; }
+        [ForeignKey("Offer")]
+        public int OfferId { get; set; }
+        [Display(Name = "Photo")]
+        [DataType(DataType.Upload)]
+        public IFormFile PhotoFile { get; set; } 
     }
 }
